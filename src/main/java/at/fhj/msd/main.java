f09874b63b420ad40648f1135142c92233e27c0b
Mario Shenouda <mario.shenouda@edu.fh-joanneum.at>
2025-03-17 23:18:52 +0100

    rename main.java

--- src/main/java/at/fhj/msd/main.java
+++ src/main/java/at/fhj/msd/main.java
@@ -1,6 +1,6 @@
 package at.fhj.msd;
 
-public class Main {
+public class main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("Addition: " + calc.add(10, 5));
