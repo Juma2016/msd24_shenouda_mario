b5b8aded3a0735d9d14f4fbe934b76d4b2e6d6a5
Mario Shenouda <mario.shenouda@edu.fh-joanneum.at>
2025-03-17 21:18:44 +0100

    add main.java

---
+++ src/main/java/at/fhj/msd/main.java
@@ -0,0 +1,11 @@
+package at.fhj.msd;
+
+public class main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraktion: " + calc.minus(10, 5));
+        System.out.println("Multiplikation: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+    }
+}
