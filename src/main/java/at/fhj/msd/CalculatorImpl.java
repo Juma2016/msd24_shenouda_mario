26cedff1bb35ddde3977496aef655e1012aed3f6
Mario Shenouda <mario.shenouda@edu.fh-joanneum.at>
2025-04-22 13:41:12 +0200

    Docs: add Javadoc to ICalculator interface and CalculatorImpl methods

--- src/main/java/at/fhj/msd/CalculatorImpl.java
+++ src/main/java/at/fhj/msd/CalculatorImpl.java
@@ -1,13 +1,22 @@
 package at.fhj.msd;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+/**
+ * CalculatorImpl.java
+ * 
+ * @author Mario Shenouda
+ * @version 1.0
+ * @since 2023-10-01
+ */
 public class CalculatorImpl implements ICalculator {
 
     private static final Logger logger = LogManager.getLogger(CalculatorImpl.class);
 
     /**
      * Easy Calculator for first steps in test automation for Educational Usage
+     */
+    /**
+     * Default constructor
      */
     public CalculatorImpl() {
 
@@ -17,6 +26,15 @@ public class CalculatorImpl implements ICalculator {
      * (non-Javadoc)
      * 
      * @see at.fhj.itm.demo.Calculator#add(java.lang.double, java.lang.double)
+     */
+    /**
+     * Adds two numbers together.
+     * 
+     * @param number1 the first number
+     * @param number2 the second number
+     * @return the sum of the two numbers
+     * @throws IllegalArgumentException if either number is not a valid number
+     * @throws ArithmeticException if the result is not a valid number
      */
     @Override
     public double add(double number1, double number2) {
@@ -29,6 +47,16 @@ public class CalculatorImpl implements ICalculator {
      * 
      * @see at.fhj.itm.demo.Calculator#minus(java.lang.double, java.lang.double)
      */
+    /**
+     * Subtracts the second number from the first number.
+     * 
+     * @param number1 the first number
+     * @param number2 the second number
+     * @return the difference of the two numbers
+     * @throws IllegalArgumentException if either number is not a valid number
+     * @throws ArithmeticException if the result is not a valid number
+     */
+
     @Override
     public double minus(double number1, double number2) {
         logger.debug("Aufruf von subtract() mit Parametern: number1 = " + number1 + ", number2 = " + number2);
@@ -41,6 +69,14 @@ public class CalculatorImpl implements ICalculator {
      * 
      * @see at.fhj.itm.demo.Calculator#divide(java.lang.double, java.lang.double)
      */
+    /** 
+   * Divides the first number by the second number.
+   *@param number1
+   * @param number2
+   * @return result of number1 / number2
+   * @throws IllegalArgumentException if number1 or number2 is not a number
+   * @throws ArithmeticException if number2 is 0
+    */
     @Override
     public double divide(double number1, double number2) {
         logger.debug("Aufruf von divide() mit parametern: number1=" + number1+", number2= "+number2);
@@ -61,6 +97,14 @@ public class CalculatorImpl implements ICalculator {
      * 
      * @see at.fhj.itm.demo.Calculator#multiply(java.lang.double, java.lang.double)
      */
+    /** 
+     * Multiplies two numbers together.
+   * @param number1
+   * @param number2
+   * @return result of number1 * number2
+   * @throws IllegalArgumentException if number1 or number2 is not a number
+   * @throws ArithmeticException if number2 is 0
+    */
     @Override
     public double multiply(double number1, double number2) {
         logger.debug("Aufruf von multiply() mit Parametern: number1 = " + number1 + ", number2 = " + number2);
@@ -69,6 +113,13 @@ public class CalculatorImpl implements ICalculator {
     }
 
     @Override
+    /**
+     * Calculates the factorial of a number.
+     * 
+     * @param n the number to calculate the factorial of
+     * @return the factorial of the number
+     * @throws IllegalArgumentException if n is negative
+     */
 public int factorial(int n) {
    if (n<0)
    return 0;
